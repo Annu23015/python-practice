a62c41ff5d5597c707a3120fdd32b1d84e11e431
Annu Kumari <annukumari152002@gmail.com>
2023-03-30 18:28:43 +0530

    calendar,triangle, celsius, python program

---
+++ javapoint-p1.py
@@ -0,0 +1,81 @@
+## find the area of trangle
+##l=int(input("enter the length of triangle"))
+##b=int(input("enter the breath of triangle"))
+##area=(l*b)//2
+##print("area=",area)
+
+##*******************************************************************************************************************
+
+##import cmath
+#### to solve the quadratric equation
+##
+##
+##a=float(input("enter the value of a"))
+##b=float(input("enter the value of b"))
+##c=float(input("enter the value of c"))
+##
+##d=(b**2)-(4*a*c)
+##sol1=(-b-(cmath.sqrt(d)))/(2*a)
+##sol2=(-b+(cmath.sqrt(d)))/(2*a)
+##print("the solution is {} and {}".format(sol1,sol2))
+
+
+##************************************************************************************************************
+##import random
+##n=int(input("enter a number"))
+##a=[]
+##for i in range(1,n):
+##    p=random.randint(1,50)
+##    a.append(p)
+##print(a)
+
+
+##**********************************************************************************************************************
+
+
+##display the calender
+##import calendar
+##mm=int(input("enter the month"))
+##yy=int(input("enter the year"))
+##print(calendar.month(yy,mm))
+
+
+
+##******************************************************************************************************************************
+
+##convert km into miles
+##kilometer=int(input("enter a number"))
+##mi=0.6213711922
+##miles=kilometer*mi
+##print("km in mile",miles)
+
+
+##***************************************************************************************************************************************************
+
+##convert celsius into fahrenheit
+##cel=float(input("enter the degree in celsius"))
+##fahranheit=(cel*1.8)+32
+##print("the %.2f dgree celsius is equal to %.2f fahrenheit"%(cel,fahranheit)) 
+
+
+##**********************************************************************************************************************************************************
+
+##yr=int(input("enter the year"))
+##if yr%400==0 or yr%100!=0 and yr%4==0:
+##    print("leap year")
+##else:
+##    print("not leap year")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
